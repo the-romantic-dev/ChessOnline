2f82dec9411243e2e7d8b442d58d9173434779dc
marzepanh <marzepanh@gmail.com>
2021-05-21 17:12:20 +0300

    Отправка хода в бд

--- app/src/main/java/org/spbstu/ysa/chessonline/online/Room.java
+++ app/src/main/java/org/spbstu/ysa/chessonline/online/Room.java
@@ -4,24 +4,16 @@ public class Room {
     private String password;
     private Boolean connection;
     private Boolean creatorIsWhite;
-    private String pawnTo;
-    private int xFrom;
-    private int yFrom;
-    private int xTo;
-    private int yTo;
+    private Move move;
 
     public Room() {
     }
 
-    public Room(String password, boolean connection, boolean isWhite, String pawnTo, int xFrom, int yFrom, int xTo, int yTo) {
+    public Room(String password, boolean connection, boolean isWhite, Move move) {
         this.password = password;
         this.connection = connection;
         this.creatorIsWhite = isWhite;
-        this.pawnTo = pawnTo;
-        this.xFrom = xFrom;
-        this.yFrom = yFrom;
-        this.xTo = xTo;
-        this.yTo = yTo;
+        this.move = move;
     }
 
     public String getPassword() {
@@ -30,46 +22,6 @@ public class Room {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getPawnTo() {
-        return pawnTo;
-    }
-
-    public void setPawnTo(String pawnTo) {
-        this.pawnTo = pawnTo;
-    }
-
-    public int getxFrom() {
-        return xFrom;
-    }
-
-    public void setxFrom(int xFrom) {
-        this.xFrom = xFrom;
-    }
-
-    public int getyFrom() {
-        return yFrom;
-    }
-
-    public void setyFrom(int yFrom) {
-        this.yFrom = yFrom;
-    }
-
-    public int getxTo() {
-        return xTo;
-    }
-
-    public void setxTo(int xTo) {
-        this.xTo = xTo;
-    }
-
-    public int getyTo() {
-        return yTo;
-    }
-
-    public void setyTo(int yTo) {
-        this.yTo = yTo;
     }
 
     public boolean getConnection() { return connection; }
@@ -82,5 +34,13 @@ public class Room {
 
     public void setCreatorColor(Boolean white) {
         creatorIsWhite = white;
+    }
+
+    public Move getMove() {
+        return move;
+    }
+
+    public void setMove(Move move) {
+        this.move = move;
     }
 }
